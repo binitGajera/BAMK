40b993db787c7b28adce0e53cc2038435090fc0a
akashdesai95 <akashdesai2007@gmail.com>
2016-09-16 12:39:18 +0530

    left:retrive data from the  data set which is from cache(temp)

---
+++ CacheApp/CacheApp/RequestData.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="RequestData.aspx.cs" Inherits="CacheApp.RequestData" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <table>
+            <tr>
+                <td><asp:Label ID="lb1" runat="server" /></td>
+            </tr>
+            <tr>
+                <td>ID:</td><td><asp:TextBox ID="tb1" runat="server" /></td>
+            </tr>
+            <tr>
+                <td><asp:Button ID="b1" Text="submit" runat="server" OnClick="Button_click" /></td>
+            </tr>
+        </table>
+    </div>
+    </form>
+</body>
+</html>
---
+++ CacheApp/CacheApp/RequestData.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Data;
+
+namespace CacheApp
+{
+    public partial class RequestData : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            String selectsql="select * from Student ";
+            String connectionStr = "Data Source=(LocalDB)\\v11.0;AttachDbFilename=C:\\Users\\user1\\Source\\Repos\\BAMK\\CacheApp\\CacheApp\\App_Data\\Database1.mdf;Integrated Security=True";
+            SqlConnection con = new SqlConnection(connectionStr);
+           SqlCommand cmd = new SqlCommand(selectsql, con);
+            DataSet ds=new DataSet();
+            SqlDataAdapter adapter=new SqlDataAdapter(cmd);
+            
+            con.Open();
+            adapter.Fill(ds,"Student" );
+            Cache.Insert("tablename",ds,null,DateTime.Now.AddMinutes(60),TimeSpan.Zero);
+          con.Close();
+            }
+        protected void Button_click(object sender,EventArgs e)
+        {
+            if(Cache["tablename"]==null)
+            {
+                lb1.Text="Data can't be retrieved from Cache";
+            }
+            else
+            {
+                DataSet temp=(DataSet)Cache["tablename"];
+
+                lb1.Text = "success";
+            }
+
+        }
+    }
+}
